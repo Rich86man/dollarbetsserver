64655a03f5f802b1742d30c5d7b2f896fa55af2e
Richard Kirk <RichardBKirk@gmail.com>
2013-09-01 13:21:53 -0700

    initial rails setup with postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dollarbetsserver::Application.config.secret_key_base = '0b7212d5e645546c7801ef8700467f748ea285b615ddd8ec425615c251bd45138c3cc8046769f49357a54c09c71fdd8c99d788d603d3d2c3ec82d62287890d3e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dollarbetsserver::Application.config.session_store :cookie_store, key: '_dollarbetsserver_session'
